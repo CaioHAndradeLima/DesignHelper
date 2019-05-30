c19c9edcac0cb290fdb5d6d804cd289618147628
Caio Henrique Andrade Lima <quehenl@500010338821-NB.local>
2019-05-30 19:58:07 -0300

    Organizando projeto

--- app/src/main/java/com/gizmin/designhelper/MainActivity.kt
+++ app/src/main/java/com/gizmin/designhelper/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.gizmin.designhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gizmin.bitstore.knowbitstore.KnowBitStoreActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, KnowBitStoreActivity::class.java))
     }
 }
