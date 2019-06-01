af470bbda98d50cb4b8b8c66efe7a64d9a0f745c
Caio <caiohandradelima@gmail.com>
2019-06-01 11:58:41 -0300

    Adiciona mais itens em InputTypeUtils

--- bitstore/src/main/java/com/gizmin/bitstore/form_product/utils/InputTypeUtils.kt
+++ bitstore/src/main/java/com/gizmin/bitstore/form_product/utils/InputTypeUtils.kt
@@ -3,6 +3,11 @@ package com.gizmin.bitstore.form_product.utils
 import android.text.InputType
 
 object InputTypeUtils {
-    const val TEXT = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
-    const val NUMBER = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_NORMAL
+
+    const val NUMBER            = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_NORMAL
+    const val NUMBER_PASSWORD   = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+
+    const val TEXT              = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
+    const val TEXT_EMAIL_ADDRESS= InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+    const val PASSWORD          = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
 }
